e75c56b1271447a25bdcaf2f7fae91931c2fdb16
Юлия Красулина <juliya94m@gmail.com>
2023-03-01 11:58:06 +0300

    fix

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,4 @@
-const getRandomNumber = (minNum = 0, maxNum = 50) => {
-  const num = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-  return num;
-};
+// eslint-disable-next-line max-len
+const getRandomNumber = (minNum = 0, maxNum = 50) => Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
 export default getRandomNumber;
